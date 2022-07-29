7f55c502f816c4a8267704d28c18a5b6e141ef84
Soichiro Miki <smiki-tky@umin.ac.jp>
2022-07-29 05:00:59 +0000

    Improve fetch error

--- src/createAuthorizedFetch.ts
+++ src/createAuthorizedFetch.ts
@@ -15,21 +15,35 @@ const createAuthorizedFetch = (
     }
     if ('Authorization' in (init?.headers ?? {}))
       throw new Error('Internal error: Authorization header cannot be set');
-    const res = await fetch(resource, {
-      ...init,
-      headers: {
-        ...(init?.headers ?? {}),
-        Authorization: `Bearer ${token}`
+    try {
+      const res = await fetch(resource, {
+        ...init,
+        headers: {
+          ...(init?.headers ?? {}),
+          Authorization: `Bearer ${token}`
+        }
+      });
+      if (!res.ok) {
+        throw new Error(
+          `HTTP error: ${res.status} ${res.statusText}\n` +
+            `URL: ${res.url}\n` +
+            `Error content: ${await res.text()}`
+        );
       }
-    });
-    if (!res.ok) {
-      throw new Error(
-        `HTTP error: ${res.status} ${res.statusText}\n` +
-          `URL: ${res.url}\n` +
-          `Error content: ${await res.text()}`
-      );
+      return res;
+    } catch (err: any) {
+      if (err instanceof TypeError) {
+        // fetch failed due to a network error, a CORS error, etc.
+        throw new Error(
+          `HTTP error: ${err.message}\n` +
+            `URL: ${resource}` +
+            ('cause' in err ? `\nCause: ${(err as any).cause}` : '')
+        );
+      } else {
+        // error with HTTP status code, e.g. 404, 500.
+        throw err;
+      }
     }
-    return res;
   };
 };
 
